e64bd9b2a064672ef6645eb071ac470ecf47a790
SpicyNachos03 <122487143+SpicyNachos03@users.noreply.github.com>
2025-02-08 14:38:04 -0500

    Post VIA Website WORKS

--- ugahacksx/app/khoaTest/page.tsx
+++ ugahacksx/app/khoaTest/page.tsx
@@ -10,7 +10,24 @@ export default function khoaTest() {
   };
 
   const handleSubmit = () => {
-    alert(`Submitted: ${inputValue}`);
+    const requestBody = {
+      text: inputValue,
+    };
+
+    fetch('/api/pinata', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(requestBody),
+    })
+    .then(response => response.json())
+    .then(data => {
+      console.log('Success:', data);
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+    });
   };
 
   return (
