84f4284625e590d7c244ebb3c0c87476afcbb8ab
laury64368 <131693578+laury64368@users.noreply.github.com>
2025-02-08 18:28:24 -0500

    home page

--- ugahacksx/app/components/genBar/page.tsx
+++ ugahacksx/app/components/genBar/page.tsx
@@ -1,14 +1,21 @@
+"use client";
 import React from "react";
-import "./genBar.module.css";
+import styles from "./genBar.module.css";
+import { useRouter } from 'next/navigation';
 
 const GenBar = () => {
+  const router = useRouter();
+  
+  const handleClick = () => {
+    router.push('pages/exampleUsage');
+  };
+
   return (
-    <div className="gen-container">
+    <div className={styles.genContainer}>
       <input type="text" placeholder="Generate sounds..." />
       <button type="submit">Submit</button>
     </div>
   );
 };
-
 
 export default GenBar;
