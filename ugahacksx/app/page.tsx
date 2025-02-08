cc9c03f850e227f5de6d11146106e1b2cea5db26
laury64368 <131693578+laury64368@users.noreply.github.com>
2025-02-08 11:58:33 -0500

    erros

--- ugahacksx/app/page.tsx
+++ ugahacksx/app/page.tsx
@@ -1,135 +1,95 @@
 import Image from "next/image";
 import styles from "./page.module.css";
-"use client";
-export const dynamic = 'force-static';
-//import NavigationBar from "./components/navBar";
-import { useState, useEffect } from "react";
-import Link from "next/link";
-//import { useUserCtx } from "./context/UserContext";
-//import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-
 
 export default function Home() {
-  //const { userData, logout } = useUserCtx();
-
-  const [currentSlide, setCurrentSlide] = useState(Array(8).fill(0));
-  const [displayedImages, setDisplayedImages] = useState([]);
-
-  const images = [
-    "cover1.png",
-    "cover2.png",
-    "cover3.png",
-    "cover4.png",
-    "cover5.png",
-    "cover6.png",
-    "cover7.png",
-    "cover8.png",
-    "cover9.png",
-    "cover10.png",
-    "cover11.png",
-    "cover12.png",
-    "cover13.png",
-    "cover14.png",
-    "cover15.png",
-    "cover16.png",
-    "cover17.png",
-    "cover18.png",
-  ];
-
-  useEffect(() => {
-    // Change displayed images initially
-    changeDisplayedImages();
-
-    // Set interval to change displayed images every 3 seconds
-    const interval = setInterval(() => {
-      changeDisplayedImages();
-    }, 3000);
-
-    // Clear interval on component unmount
-    return () => clearInterval(interval);
-  }, []);
-
-  const changeDisplayedImages = () => {
-    const randomImages = [];
-    const availableImages = [...images]; // Create a copy of the images array
-
-    for (let i = 0; i < 8; i++) {
-      const randomIndex = Math.floor(Math.random() * availableImages.length);
-      const selectedImage = availableImages[randomIndex];
-      randomImages.push(selectedImage);
-      // Remove the selected image from the available images
-      availableImages.splice(randomIndex, 1);
-    }
-
-    //setDisplayedImages(randomImages);
-  };
-
   return (
-    <div className={styles.hpContainer}>
-      <nav className={styles.navBar}>
-        <ul className={styles.navList}>
-              <li className={styles.navItem}>
-                <Link href="/createAccount">
-                  <button className={styles.navButton}>Create Account</button>
-                </Link>
-              </li>
-              <li className={styles.navItem2}>
-                <Link href="/login">
-                  <button className={styles.navButton}>Login</button>
-                </Link>
-              </li>
-            <>
-              <li>
-                <Link href="/playlist">
-                  <button className={styles.navButton}>Playlist</button>
-                </Link>
-              </li>
-              <li>
-                <button className={styles.navButton} >Logout</button>
-              </li>
-            </>
-        </ul>
-      </nav>
-
-      <div className={styles.welcomeBanner}>
-        <div className={styles.welcomeTextContainer}>
-          <div className={styles.welcomeText}>
-            <h1>
-              {" "}
-              Welcome to <br /> SoundPalette{" "}
-            </h1>
-            <h2> Where playlists paint your mood </h2>
-          </div>
-        </div>
-        <div className={styles.overlay}></div>
-        <img
-          src="Images/HPBanner.gif"
-          alt="banner"
-          className={styles.bannerImage}
+    <div className={styles.page}>
+      <main className={styles.main}>
+        <Image
+          className={styles.logo}
+          src="/next.svg"
+          alt="Next.js logo"
+          width={180}
+          height={38}
+          priority
         />
-      </div>
-      <div className={styles.searchBarContainer}>
-      </div>
-      <p> See what's trending near you</p>
+        <ol>
+          <li>
+            Get started by editing <code>app/page.tsx</code>.
+          </li>
+          <li>Save and see your changes instantly.</li>
+        </ol>
 
-      <div className={styles.gridContainer}>
-        {displayedImages.map((imageName, index) => (
-          <div key={index} className={styles.gridCell}>
-            <div className={styles.imageContainer}>
-              <Image
-                src={`/images/${imageName}`}
-                alt={`Image ${index}`}
-                width={150}
-                height={150}
-              />
-            </div>
-          </div>
-        ))}
-      </div>
-
-      <Link href="/createAccount">
-        <button className={styles.mixButton}>Start mixing your playlist</button>
-      </Link>
+        <div className={styles.ctas}>
+          <a
+            className={styles.primary}
+            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image
+              className={styles.logo}
+              src="/vercel.svg"
+              alt="Vercel logomark"
+              width={20}
+              height={20}
+            />
+            Deploy now
+          </a>
+          <a
+            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+            className={styles.secondary}
+          >
+            Read our docs
+          </a>
+        </div>
+      </main>
+      <footer className={styles.footer}>
+        <a
+          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/file.svg"
+            alt="File icon"
+            width={16}
+            height={16}
+          />
+          Learn
+        </a>
+        <a
+          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/window.svg"
+            alt="Window icon"
+            width={16}
+            height={16}
+          />
+          Examples
+        </a>
+        <a
+          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/globe.svg"
+            alt="Globe icon"
+            width={16}
+            height={16}
+          />
+          Go to nextjs.org →
+        </a>
+      </footer>
     </div>
   );
 }
