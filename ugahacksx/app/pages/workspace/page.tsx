2574143f71af80db1366445f52fdef6e6600034d
laury64368 <131693578+laury64368@users.noreply.github.com>
2025-02-09 02:04:13 -0500

    play bar component created

--- ugahacksx/app/pages/workspace/page.tsx
+++ ugahacksx/app/pages/workspace/page.tsx
@@ -4,9 +4,8 @@ import Card from "../../components/WorkspaceCard"
 import styles from "./workspace.module.css";
 import GenBar from "../../components/genBar/page";
 import NavBar from "../../components/NavBar/page";
-import PLAY from '../../public/playButton.png';
-import HEART from '../../public/heart1.jpg';
-import Image from "next/image";
+import PlayBar from "../../components/playBar/page";
+
 
 export default function Workspace() {
   return (
@@ -26,35 +25,7 @@ export default function Workspace() {
     <section className = {styles.container2}>
     <h2>Create songs for your mood</h2>
     </section>
-    
-    <footer className={styles.workfooter}>
-
-        <Image 
-        className={styles.image}
-        src={PLAY}
-        alt="play button"
-        width={50}
-        height={50}
-      />
-      <Image 
-        className={styles.image}
-        src={HEART}
-        alt="heart"
-        width={70}
-        height={70}
-      />
-      <div className="playback-bar">
-          <span className="current-time">00:00</span>
-          <input
-        type="range"
-        min="0"
-        max="100"
-        className={styles.progressBar}
-        step="1"
-          />
-          <span className="curt-time">0:30</span>
-        </div>
-     </footer>
+    <PlayBar />
     </div>
   );
 }
