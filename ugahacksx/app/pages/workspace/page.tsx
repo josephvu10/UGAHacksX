f1c90e3b19cd7bd4ccc2c35062f5d6826cd1ebe3
laury64368 <131693578+laury64368@users.noreply.github.com>
2025-02-09 00:56:56 -0500

    css

--- ugahacksx/app/pages/workspace/page.tsx
+++ ugahacksx/app/pages/workspace/page.tsx
@@ -4,6 +4,10 @@ import Card from "../../components/WorkspaceCard"
 import styles from "./workspace.module.css";
 import GenBar from "../../components/genBar/page";
 import NavBar from "../../components/NavBar/page";
+import PLAY from '../../public/playButton.png';
+import HEART from '../../public/heart1.jpg';
+import Image from "next/image";
+
 export default function Workspace() {
   return (
     <div>
@@ -24,8 +28,32 @@ export default function Workspace() {
     </section>
     
     <footer className={styles.workfooter}>
-        <p>&copy; 2023 My Workspace. All rights reserved.</p>
-        <img src="Images/playButton.png" alt="Logo" className={styles.logo} />
+
+        <Image 
+        className={styles.image}
+        src={PLAY}
+        alt="play button"
+        width={50}
+        height={50}
+      />
+      <Image 
+        className={styles.image}
+        src={HEART}
+        alt="heart"
+        width={70}
+        height={70}
+      />
+      <div className="playback-bar">
+          <span className="current-time">00:00</span>
+          <input
+        type="range"
+        min="0"
+        max="100"
+        className={styles.progressBar}
+        step="1"
+          />
+          <span className="curt-time">0:30</span>
+        </div>
      </footer>
     </div>
   );
