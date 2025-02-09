08a6d234144330e2442fd9fd2b3a6ed6373f94df
SpicyNachos03 <122487143+SpicyNachos03@users.noreply.github.com>
2025-02-08 20:06:50 -0500

    Configured Auth0

--- ugahacksx/app/page.tsx
+++ ugahacksx/app/page.tsx
@@ -2,8 +2,12 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import { useRouter } from 'next/navigation';
+import { useUser } from '@auth0/nextjs-auth0/client';
 
 export default function Home() {
+  // For Auth0
+  const { user, error, isLoading } = useUser();
+
   const router = useRouter();
 
   const handleClick = () => {
@@ -22,25 +26,40 @@ export default function Home() {
   const khoaTest = () => {
     router.push('/khoaTest');
   }
+
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>{error.message}</div>;
   return (
     <div className={styles.page}>
-      <ul className= {styles.list}>
-      <li className={styles.listItem}><button className={styles.button} onClick={handleClick}>Go to Example Usage</button></li>
-      <li className={styles.listItem}><button className={styles.button} onClick={handleClickSongPosts}>Go to Public Songs</button></li>
-      <li className={styles.listItem}><button className={styles.button} onClick={handleClickWorkspace}>Go to Workspace</button></li>
-      <li className={styles.listItem}><button className={styles.button} onClick={khoaTest}>Khoa's Test</button></li>
-      <a className={styles.link} href="/api/auth/login">Login</a>
-      <a className={styles.link} href="/api/auth/logout">Logout</a>
+      <ul className={styles.list}>
+        <li className={styles.listItem}><button className={styles.button} onClick={handleClick}>Go to Example Usage</button></li>
+        <li className={styles.listItem}><button className={styles.button} onClick={handleClickSongPosts}>Go to Public Songs</button></li>
+        <li className={styles.listItem}><button className={styles.button} onClick={handleClickWorkspace}>Go to Workspace</button></li>
+        <li className={styles.listItem}><button className={styles.button} onClick={khoaTest}>Khoa's Test</button></li>
+        <a className={styles.link} href="/api/auth/login">Login</a>
+        <a className={styles.link} href="/api/auth/logout">Logout</a>
       </ul>
 
-      <div className = {styles.titleContainer}> 
-      <h1 className={styles.title}> Slogan slogan slogan slogan </h1>
+      <div className={styles.titleContainer}>
+        <h1 className={styles.title}> Slogan slogan slogan slogan </h1>
       </div>
 
-        <div className={styles.genBarContainer}>
-          <input type="text" placeholder="Generate sounds..." />
-          <button type="submit">Submit</button>
+      <div className={styles.genBarContainer}>
+        <input type="text" placeholder="Generate sounds..." />
+        <button type="submit">Submit</button>
       </div>
+      {user ? (
+        <div>
+          <div>
+            <img src={user.picture} alt={user.name} />
+          </div>
+          <div>
+            <h2>{user.name}</h2>
+            <p>{user.email}</p>
+          </div>
+          <pre>{JSON.stringify(user, null, 2)}</pre>
+        </div>
+      ) : null}
 
     </div>
 
